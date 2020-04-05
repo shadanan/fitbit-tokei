a917d0abeab871f8d5a4b2a521900e539defc8ef
Shad Sharma <shadanan@gmail.com>
2020-04-05 00:00:48 -0700

    Wire up date + placeholder for heart rate.

--- app/index.ts
+++ app/index.ts
@@ -1,21 +1,51 @@
 import document from "document";
 import clock from "clock";
 
-function zeropad(number: number): string {
-  return ("0" + number).slice(-2);
+let _date = "APR 8";
+let _temperature = "25°C";
+const MONTH_NAMES = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+function setDate(date: Date) {
+  _date = `${MONTH_NAMES[date.getMonth()]} ${date.getDate()}`;
+  updateDateAndTemperature();
+}
+
+function setTemperature(temperature: number) {
+  _temperature = `${temperature}°C`;
+  updateDateAndTemperature();
+}
+
+function updateDateAndTemperature() {
+  const dateAndTempText = document.getElementById("dateAndTemperature");
+  dateAndTempText.text = `${_date}  •  ${_temperature}`;
 }
 
 function format12Hour(date: Date): string {
   const hours = date.getHours() % 12 || 12;
-  const minutes = zeropad(date.getMinutes());
+  const minutes = ("0" + date.getMinutes()).slice(-2);
   return `${hours}:${minutes}`;
 }
 
-let clockText = document.getElementById("clockText");
+function setTime(date: Date) {
+  const clockText = document.getElementById("clockText");
+  clockText.text = `${format12Hour(date)}`;
+}
 
 clock.granularity = "minutes"; // seconds, minutes, hours
-
 clock.ontick = function (evt) {
-  const date = evt.date;
-  clockText.text = `${format12Hour(date)}`;
+  setDate(evt.date);
+  setTime(evt.date);
 };
